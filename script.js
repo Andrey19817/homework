5d743a93c38b58b6f09152371fc30b1aef5715ba
andrey19817 <andrey19817@mail.ru>
2021-10-01 13:14:27 +0300

    111

---
+++ script.js
@@ -0,0 +1,75 @@
+// 1. ПОЛЬЗОВАТЕЛЬ ВВОДИТ ВОЗРАСТ ФЕДОТА и МИТРОФАНА
+// выяснить кто старше фдеот или митрофан и НА СКОЛЬКО старше
+// (возрасты могут быть равны)
+// вывести результат в консоль
+
+// 2. Пользователь вводит три числа, выяснить наибольшее из них
+
+// 3. пользователь вводит 4 числа
+// выяснить СКОКЛЬКО из этих чисел были четными
+
+
+// 1.
+// let mitrofanAge = prompt('mitrofan');
+// let fedotAge = prompt('fedot');
+
+// if ( isNaN(mitrofanAge) || isNaN(fedotAge) ) {
+//     console.log('incrorrect data');
+// } else if ( mitrofanAge === null || fedotAge === null ) {
+//     console.log('you cancelled');
+// } else {
+//     mitrofanAge = +mitrofanAge;
+//     fedotAge = +fedotAge;
+
+//     const ageDifference = Math.abs(mitrofanAge - fedotAge);
+
+//     if ( mitrofanAge > fedotAge ) {
+//         console.log(`Mitrofan is older than Fedot by ${ageDifference} years`);
+//     } else if (fedotAge > mitrofanAge) {
+//         console.log(`Fedot is older than Mitrofan by ${ageDifference} years`);
+//     } else {
+//         console.log('Their ages are equal');
+//     }
+// }
+
+
+// 2.
+
+// let a = +prompt('one');
+// let b = +prompt('two');
+// let c = +prompt('three');
+
+// if (a >= c && a >= b) {
+//     console.log(a);
+// } else if (b > a && b > c) {
+
+// } else {
+
+// }
+
+// 3. 
+// let a = +prompt('one'); // 1
+// let b = +prompt('two'); // 2
+// let c = +prompt('three'); // 1
+// let d = +prompt('four'); // 2
+
+// let evenCounter = 0;
+
+// if (a % 2 === 0) {
+//     evenCounter++;
+// }
+
+// if (b % 2 === 0) {
+//     evenCounter++;
+// }
+
+// if (c % 2 === 0) {
+//     evenCounter++;
+// }
+
+// if (d % 2 === 0) {
+//     evenCounter++;
+// }
+
+// console.log(evenCounter);
+console.log(a+b);
