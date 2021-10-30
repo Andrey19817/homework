b332e36a2ca668258f3d32e234d57649c9fe047d
andrey19817 <andrey19817@mail.ru>
2021-10-30 23:52:10 +0300

    dz-5

--- functions/script.js
+++ functions/script.js
@@ -199,3 +199,188 @@
 //     result(sum,i);
 // }
 // getNum();
+// ----------------------------------------------------------------------------------------
+
+// 1. Создать функцию «переключатель» toggleMe(),
+// которая при вызове чередует возвращаемые булевы
+// значения (true, false).
+
+
+// function toggleMe(){
+//     let val=true;
+//     return function(){
+//         val=!val;
+//         return val;
+//     } 
+// }let marcker=toggleMe();
+// console.log(marcker());
+// console.log(marcker());
+// console.log(marcker());
+// console.log(marcker());
+
+// 2. Создать функцию funcCounter(), которая способна
+// считать количество своих вызовов. Если передать в эту
+// функцию число меньше 100, то функция считает
+// количество вызовов от этого числа до бесконечности,
+// если передать число больше 100, то функция считает
+// количество от этого числа до 0
+
+
+// let a=100
+// function funcCounter1(a){
+//     let sum=a;
+//     return function(){
+//         if(a<=100){
+//             sum++;
+//             return sum;
+//         }else if(a>100 && sum>0){
+//             sum--;
+//             return sum;
+//         }
+//     }
+//     }
+//     let funcCounter=funcCounter1(a);
+    
+//     for(let i=0;;i++){
+//         console.log(funcCounter());
+//         if(i===120){
+//             break;
+//         }
+//     }
+
+
+
+
+// 3. Создать функцию classNameEditor(), которая
+// принимает строку с названием класса и добавляет
+// его в класс, если вторым параметром было
+// передано true, если вторым параметром было
+// передано false, то функция пытается удалить из
+// класса переданное имя. Если удаление не было
+// совершено, то просто вернуть строку с названием
+// класса. Функция должна возвращать текущее
+// название класса.
+
+// let className ='class';
+
+// function classNameEditor(text1,text2){
+    
+//     className=text1 + className;
+//     if(text2===true || text2===false){
+//     let arr=className.split('');
+//     for(let i=0;i<text1.length;i++){
+//         arr.shift(arr[i]);
+//     } 
+//       className=arr.join('');
+      
+//     }return className; 
+// }
+
+// console.log(classNameEditor('lhghg'));
+// console.log(classNameEditor('zJHJ',true));
+// console.log(classNameEditor('zJHJ'));
+
+// ----------------------------------------------------------------------------------------------------
+
+
+// 1. Даны 2 массива:
+// let people1 = ['Samuel', 'Jack', 'Thomas','Henry','Leo','Connor','David','Ryan'];
+// let people2 = ['Connor','Stanley','Leo', 'Albert','Owen','Oliver','Ethan','Thomas'];
+
+// 1.1 Пользователь вводит имя, если это имя есть в массиве people1, добавьте это имя в конец people2.
+// 1.2 Пользователь вводит имя, если это имя есть в массиве people2, удалите это имя из массива.
+// 1.3 Создайте массив только из тех имен, которые совпадают в обоих массивах.
+// 1.4 Объедините массивы people1 и people2 так, чтобы в получившемся массиве не было одинаковых имен.
+// 1.5 Отсортируйте получившийся массив по алфавиту
+
+// let people1 = ['Samuel', 'Jack', 'Thomas','Henry','Leo','Connor','David','Ryan'];
+// let people2 = ['Connor','Stanley','Leo', 'Albert','Owen','Oliver','Ethan','Thomas'];
+
+
+// 1.1 Пользователь вводит имя, если это имя есть в массиве people1, добавьте это имя в конец people2.
+
+// function additionName(){
+//     let a=prompt('введите искомое имя');
+//     if(people1.includes(a,0)){
+//         people2.push(a);
+//     }return people2;
+// }
+// console.log(additionName());
+
+
+
+// 1.2 Пользователь вводит имя, если это имя есть в массиве people2, удалите это имя из массива.
+
+// function deletionName(){
+//     let a=prompt('введите искомое имя');
+//     let findName=people2.indexOf(a,0);
+//     if(findName >= 0){
+//         people2.splice(findName,1);
+//         return people2;
+//     }else{
+//         alert('такого имени нет');
+//     }
+// }
+// console.log(deletionName());
+
+
+// 1.3 Создайте массив только из тех имен, которые совпадают в обоих массивах.
+
+// function coincidenceName(){
+//     let newPeople=[];
+//     for(let i=0;i<people1.length;i++){
+//         if(people2.indexOf(people1[i],0)>=0){
+//             newPeople.push(people1[i])
+//         }
+//     }return newPeople
+// }
+//     console.log(coincidenceName());
+
+// -----------------------------------------------------------------------------------------
+
+// 2. Дан случайный массив чисел. С помощью метода .filter()
+// отфильтруйте массив так, чтобы в новый массив вошли
+// только четные двухзначные числа из исходного.
+
+// let arr=[23,45,64,122,78,56,47,89,90];
+// let result = arr.filter(item => item > 10 && item < 100 && item % 2=== 0);
+// console.log(result);
+
+// ----------------------------------------------------------------------------------------------
+
+// 3. Дан случайный массив имен. Выяснить есть ли в этом
+// массиве «Семён» с помощью метода .some().
+
+// let arr=['Семен','Степан','Николай','Виктор'];
+// let result=arr.some(elem => elem ==='Степан');
+// console.log(result);
+
+// ----------------------------------------------------------------------------------------------------
+
+// 4. Дан массив случайных чисел. С помощью метода
+// .reduce() просуммировать только четные числа из этого
+// массива.
+
+// let arr=[23,45,64,122,78,56,47,89,90];
+// let result=arr.reduce(function(sum,curent){
+//     if(curent % 2 ===0){
+//         return sum + curent;
+//     }else{
+//         return sum;
+//     }
+// })
+// console.log(result);
+
+// ------------------------------------------------------------------------------------------------
+// 5. Пользователь вводит 10 случайных значений. Каждое
+// значение необходимо записать в массив. С помощью
+// метода .every() проверить были ли все введенные
+// пользователем данные – числами.
+
+// let arr=[];
+// for(let i=0;i<10;i++){
+//     let a=prompt('введите число');
+//     arr.push(a);
+// }
+// let result=arr.every(elem=>Number(elem));
+// console.log(result);
