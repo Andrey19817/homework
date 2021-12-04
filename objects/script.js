d8af518943e5e86e7e3334fcbc8b5911d357bc60
andrey19817 <andrey19817@mail.ru>
2021-12-04 22:36:04 +0300

    DZ-9

--- objects/script.js
+++ objects/script.js
@@ -467,3 +467,178 @@
 // let timer1 = new Timer();
 // timer1.start();
 // setTimeout(timer1.stop, 5000, timer1.intervalId);
+
+
+// ===========================DZ-9==================================
+// Создать класс Shop, инстанс которого обладает полем
+// shopItems - массив объектов типа shopItem
+// У инстанса класса Shop должен быть метод setSales(discountPercent),
+// который устанавливает всем shopItem'ам isOnSale = true,
+// а также снижает цену на discountPercent процентов.
+// У инстанса также должен быть метод purchaseItem(itemName)
+// которое находит в массиве shopItems, необходимый shopItem и уменьшает его
+// количество на 1.
+// Если количество shopItem равно нулю, то нужно выбросить ошибку о том, что
+// айтемов больше нет в наличии. Обработайте эту ошибку и выведите в консоль
+// сообщение что айтем нельзя заказать тк он закончился.
+// У инстанса Shop также должно быть поле totalProfit - число (общее
+// количество заработанных денег), в которое складывается
+// цена каждого проданного айтема.
+
+// Создать класс ShopItem, инстанс которого должен обладать полями
+// itemName - название товара
+// price - цена товара (не может быть меньше нуля, используйте сеттер)
+// amount - количество товара (не может быть меньше нуля, используйте сеттер)
+// isOnSale - булево значение
+
+// Создать класс Clothes - который наследует от ShopItem
+// инстанс должен обладать дополнительными полями
+// size - строка характеризующая размер (может принимать значения только 'S', 'M', 'L', 'XL', обрабатывать
+// сеттером, иначе выбрасывать ошибку)
+// material - строка характеризующая материал
+// color - строка цвета
+// Класс Clothes, должен обладать статическим методом .changeColor(cloth, color)
+// который принимает инстанс этого класса и цвет, метод меняет инстансу цвет на color;
+
+// Создать класс TV который наследует от ShopItem
+// инстанс должен обладать дополнительными полями
+// diagonal - число
+// withSmartTV - булево значение
+// Оба поля должны быть недоступны для изменения, но доступны для чтения
+// т.е. значения этих полей устанавливаются только в конструкторе, дальше сеттерами
+// запрещаем их изменять
+
+// Создать инстанс Shop и прокинуть в него несколько инстансов TV и Clothes
+// проверить все методы на работоспособность
+
+// --------------------------------------------------------------------
+// class Shop {
+//     constructor() {
+//         this.shopItems = [];
+//         this.totalProfit = 0;
+//     }
+//     setSales(discauntPercent) {
+//         this.isOnSale = true;
+//         this.shopItems.forEach(elem => elem._price -= discauntPercent)
+//     }
+//     purchaseItem(itemName) {
+//         let a = 0;
+//         this.shopItems.find(function (elem) {
+//             if (elem.itemName === itemName && elem._amount > 0) {
+//                 elem._amount--;
+//                 a += elem.price
+//             } else if (elem._amount <= 0) {
+//                 elem._amount = 0;
+//                 throw new Error('finished product')
+//             }
+//         }
+//         );
+//         this.totalProfit += a;
+//     }
+//     pushShopItems(shopItem) {
+//         this.shopItems.push(shopItem)
+//     }
+// }
+// class ShopItem {
+//     constructor(itemName, price) {
+//         this.itemName = itemName;
+//         if(price <= 0){
+//             throw new Error('the price cannot be less than zero')
+//         }else{
+//             this._price = price;
+//         }
+//         this._amount = 0;
+//         this.isonSale = true;
+//     }
+//     set price(price) {
+//         if (price > 0) {
+//             this._price = price;
+//         } else {
+//             throw new Error('the price cannot be less than zero')
+//         }
+//     }
+//     get price() {
+//         console.log(this._price);
+//     }
+//     set amount(amount) {
+//         if (amount === 0) {
+//             this.isOnSale = false;
+//             console.log('no product');
+//         } else {
+//             this._amount = amount;
+//         }
+//     }
+//     get amount() {
+//         console.log(this._amount);
+//     }
+
+// }
+// class Clothes extends ShopItem {
+//     constructor(itemName, price, material, color) {
+//         super(itemName, price);
+//         this._size = '';
+//         this.material = material;
+//         this.color = color;
+//     }
+//     set size(size) {
+//         if (size === 'S' || size === 'M' || size === 'L' || size === 'XL') {
+//             this._size = size;
+//         } else {
+//             console.log('error size');
+//         }
+//     }
+//     get size() {
+//         console.log(this._size);
+//     }
+//     static changeColor(cloth, color) {
+//         cloth.color = color
+//     }
+// }
+// class TV extends ShopItem {
+//     constructor(itemName, price, diogonal, withSmartTV) {
+//         super(itemName, price);
+//         this._diogonal = diogonal;
+//         this._withSmartTV = withSmartTV
+//     }
+//     set diogonal(diogonal) {
+//         throw new Error(`Error: diogonal ${this._diogonal}`)
+//     }
+//     get diogonal() {
+//         return `${this._diogonal}`
+//     }
+//     set withSmartTV(withSmartTV) {
+//         throw new Error(`withSmartTV ${this._withSmartTV}`)
+//     }
+//     get withSmartTV(){
+//         return `${this._withSmartTV}`
+//     }
+// }
+
+// let shop1 = new Shop()
+// // console.log(Clothes);
+// let clothes1 = new Clothes('pants', 100, 'cloth', 'red')
+// clothes1.size = 'S';
+// clothes1.amount = 10;
+// let clothes2 = new Clothes('jacket', 50, 'leather', 'blue')
+// clothes2.amount = 3;
+// clothes2.size = 'XL'
+// // console.log(clothes1);
+// shop1.pushShopItems(clothes1)
+// shop1.pushShopItems(clothes2)
+// shop1.setSales(10)
+// // shop1.purchaseItem('jacket')
+// // shop1.purchaseItem('pants')
+// // shop1.purchaseItem('jacket')
+// // shop1.purchaseItem('jacket')
+// // shop1.purchaseItem('jacket')
+// // console.log(clothes2);
+// // console.log(clothes1);
+// // console.log(shop1);
+// // console.log(shop1.purchaseItem('jacket'));
+// // Clothes.changeColor(clothes1, 'black');
+// // console.log(clothes1);
+// let tv1 = new TV('Samsung', 1000, 101, true);
+// tv1.amount = 10;
+// // tv1.price = 0
+// // tv1.diogonal = 80;
+// // tv1.withSmartTV = false
